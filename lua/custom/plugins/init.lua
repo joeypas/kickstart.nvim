a543fd765b052ed18bfdc811ec08e58e27058a35
Joe Liotta <jliotta03@gmail.com>
2024-10-04 13:32:12 -0400

    slight refactor

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,225 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'max397574/better-escape.nvim',
+    config = function()
+      require('better_escape').setup()
+    end,
+  },
+  {
+    'ziglang/zig.vim',
+    ft = 'zig',
+  },
+  {
+    'R-nvim/R.nvim',
+    config = function()
+      local opts = {
+        rconsole_width = 0,
+        rconsole_height = 25,
+      }
+      require('r').setup(opts)
+    end,
+    ft = { 'R', 'Rmd' },
+  },
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    opts = {
+      icons_enabled = true,
+      theme = 'gruvbox',
+      tabline = {
+        lualine_a = { 'buffers' },
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = { 'tabs' },
+      },
+    },
+  },
+  {
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
+    config = true,
+    -- this is equalent to setup({}) function
+  },
+  {
+    'akinsho/toggleterm.nvim',
+    version = '*',
+    event = 'VeryLazy',
+    config = function()
+      require('toggleterm').setup {
+        open_mapping = [[<c-\>]],
+        direction = 'float',
+      }
+      local set_opfunc = vim.fn[vim.api.nvim_exec(
+        [[
+        func s:set_opfunc(val)
+          let &opfunc = a:val
+        endfunc
+        echon get(function('s:set_opfunc'), 'name')
+      ]],
+        true
+      )]
+
+      vim.keymap.set('', '<localleader>l', '<cmd>ToggleTermSendCurrentLine<cr>', { desc = 'Send [L]ine' })
+      vim.keymap.set('n', '<leader>tv', '<cmd>ToggleTerm size=80 direction=vertical<cr>', { desc = 'Open [V]ertical' })
+      vim.keymap.set('v', '<localleader>v', '<cmd>ToggleTermSendVisualLines<cr>', { desc = 'Send [V]isual' })
+      vim.keymap.set('n', '<localleader>m', function()
+        set_opfunc(function(motion_type)
+          require('toggleterm').send_lines_to_terminal(motion_type, false, { args = vim.v.count })
+        end)
+        vim.api.nvim_feedkeys('g@', 'n', false)
+      end, { desc = 'Send [M]otion' })
+    end,
+  },
+  {
+    'folke/trouble.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    opts = {},
+    cmd = 'Trouble',
+    keys = function()
+      return {
+        { '<leader>xx', '<cmd>Trouble diagnostics toggle<cr>', desc = 'Diagnostics' },
+        { '<leader>xl', '<cmd>Trouble loclist toggle<cr>', desc = 'Location List' },
+        { ']t', require('trouble').next, desc = 'Next Item' },
+        { '[t]', require('trouble').prev, desc = 'Prev Item' },
+      }
+    end,
+  },
+  {
+    'chentoast/marks.nvim',
+    opts = {},
+  },
+  {
+    'doctorfree/cheatsheet.nvim',
+    event = 'VeryLazy',
+    dependencies = {
+      { 'nvim-telescope/telescope.nvim' },
+      { 'nvim-lua/popup.nvim' },
+      { 'nvim-lua/plenary.nvim' },
+    },
+    config = function()
+      local ctactions = require 'cheatsheet.telescope.actions'
+      require('cheatsheet').setup {
+        bundled_cheetsheets = {
+          enabled = { 'default', 'lua', 'markdown', 'regex', 'netrw', 'unicode' },
+          disabled = { 'nerd-fonts' },
+        },
+        bundled_plugin_cheatsheets = {
+          enabled = {
+            'auto-session',
+            'goto-preview',
+            'octo.nvim',
+            'telescope.nvim',
+            'vim-easy-align',
+            'vim-sandwich',
+          },
+          disabled = { 'gitsigns' },
+        },
+        include_only_installed_plugins = true,
+        telescope_mappings = {
+          ['<CR>'] = ctactions.select_or_fill_commandline,
+          ['<A-CR>'] = ctactions.select_or_execute,
+          ['<C-Y>'] = ctactions.copy_cheat_value,
+          ['<C-E>'] = ctactions.edit_user_cheatsheet,
+        },
+      }
+    end,
+  },
+  {
+    'nvim-treesitter/nvim-treesitter-textobjects',
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    config = function()
+      require('nvim-treesitter.configs').setup {
+        textobjects = {
+          select = {
+            enable = true,
+
+            -- Automatically jump forward to textobj, similar to targets.vim
+            lookahead = true,
+
+            keymaps = {
+              -- You can use the capture groups defined in textobjects.scm
+              ['af'] = '@function.outer',
+              ['if'] = '@function.inner',
+              ['ac'] = '@class.outer',
+              -- You can optionally set descriptions to the mappings (used in the desc parameter of
+              -- nvim_buf_set_keymap) which plugins like which-key display
+              ['ic'] = { query = '@class.inner', desc = 'Select inner part of a class region' },
+              -- You can also use captures from other query groups like `locals.scm`
+              ['as'] = { query = '@scope', query_group = 'locals', desc = 'Select language scope' },
+            },
+            -- You can choose the select mode (default is charwise 'v')
+            --
+            -- Can also be a function which gets passed a table with the keys
+            -- * query_string: eg '@function.inner'
+            -- * method: eg 'v' or 'o'
+            -- and should return the mode ('v', 'V', or '<c-v>') or a table
+            -- mapping query_strings to modes.
+            selection_modes = {
+              ['@parameter.outer'] = 'v', -- charwise
+              ['@function.outer'] = 'V', -- linewise
+              ['@class.outer'] = '<c-v>', -- blockwise
+            },
+            -- If you set this to `true` (default is `false`) then any textobject is
+            -- extended to include preceding or succeeding whitespace. Succeeding
+            -- whitespace has priority in order to act similarly to eg the built-in
+            -- `ap`.
+            --
+            -- Can also be a function which gets passed a table with the keys
+            -- * query_string: eg '@function.inner'
+            -- * selection_mode: eg 'v'
+            -- and should return true or false
+            include_surrounding_whitespace = true,
+          },
+          move = {
+            enable = true,
+            set_jumps = true, -- whether to set jumps in the jumplist
+            goto_next_start = {
+              [']f'] = '@function.outer',
+              [']]'] = { query = '@class.outer', desc = 'Next class start' },
+              --
+              -- You can use regex matching (i.e. lua pattern) and/or pass a list in a "query" key to group multiple queries.
+              [']o'] = '@loop.*',
+              -- ["]o"] = { query = { "@loop.inner", "@loop.outer" } }
+              --
+              -- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
+              -- Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
+              [']s'] = { query = '@scope', query_group = 'locals', desc = 'Next scope' },
+              [']z'] = { query = '@fold', query_group = 'folds', desc = 'Next fold' },
+            },
+            goto_next_end = {
+              [']f'] = '@function.outer',
+              [']['] = '@class.outer',
+            },
+            goto_previous_start = {
+              ['[f'] = '@function.outer',
+              ['[['] = '@class.outer',
+            },
+            goto_previous_end = {
+              ['[F'] = '@function.outer',
+              ['[]'] = '@class.outer',
+            },
+            -- Below will go to either the start or the end, whichever is closer.
+            -- Use if you want more granular movements
+            -- Make it even more gradual by adding multiple queries and regex.
+            goto_next = {
+              [']c'] = '@conditional.outer',
+            },
+            goto_previous = {
+              ['[c'] = '@conditional.outer',
+            },
+          },
+        },
+      }
+      local ts_repeat_move = require 'nvim-treesitter.textobjects.repeatable_move'
+
+      vim.keymap.set({ 'n', 'x', 'o' }, ';', ts_repeat_move.repeat_last_move_next)
+      vim.keymap.set({ 'n', 'x', 'o' }, ',', ts_repeat_move.repeat_last_move_previous)
+    end,
+  },
+}
+-- vim: ts=2 sts=2 sw=2 et
