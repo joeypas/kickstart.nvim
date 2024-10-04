10b370c3e5c09be9f9de9ec7709b1d2b5a116c12
Joe Liotta <jliotta03@gmail.com>
2024-10-04 14:19:18 -0400

    small thing

--- init.lua
+++ init.lua
@@ -560,9 +560,7 @@ require('lazy').setup({
       local servers = {
         clangd = {},
         zls = {},
-        gopls = {},
-        rust_analyzer = {},
-
+        jedi_language_server = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
